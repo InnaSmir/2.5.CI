d9faa07454b990d78f1da6c09ff001bc340ba06b
Inna Smirnova <moipr@yandex.ru>
2020-06-30 02:56:15 +0300

    New test added

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -1,4 +1,4 @@
-package ru.netology.statistic;
+package ru.netology;
 
 import org.junit.jupiter.api.Test;
 import ru.netology.StatisticsService;
@@ -8,11 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class StatisticsServiceTest {
 
     @Test
-    void findMax() {
+    void findMax2018() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax2019() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 14, 17};
+        long expected = 17;
 
         long actual = service.findMax(incomesInBillions);
 
